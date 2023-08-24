b0bb73eca32db12f845ad774f1e81579e61dfe21
Sergei Voronkov <sergei.voronkov@stosolutions.com>
2023-08-24 21:41:47 +0200

    Add project files.

---
+++ AccessAwsWithNonDefaultRole/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AccessAwsWithNonDefaultRole
+{
+    using Amazon.SimpleSystemsManagement;
+    using Amazon.SimpleSystemsManagement.Model;
+    using Newtonsoft.Json;
+
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            AmazonSimpleSystemsManagementClient client = new();
+
+            GetParameterResponse response = await client.GetParameterAsync(
+                new GetParameterRequest
+                {
+                    Name = "/exl/stage/NReco/Owner",
+                    WithDecryption = true,
+                });
+
+            Console.WriteLine(
+                JsonConvert.SerializeObject(
+                    response,
+                    Formatting.Indented));
+        }
+    }
+}
